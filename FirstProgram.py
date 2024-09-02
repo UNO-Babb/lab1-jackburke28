16f53e17014076d35676493776ca97292a284ca0
jackburke28 <jackburke@unomaha.edu>
2024-09-02 05:12:35 +0000

    Finished Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jack Burke
+#Date: 09/01/2024
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  uname = input("What is your name")
   #Use the user's name in the program.
-
+  print(uname + "is smart")
   #Ask the user for their age.
-
+  age = int(input("what is your age"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
+  year = (2024-age)
 
+  print(uname + "is born in year" + str(year))
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,19 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jack Burke
+#Date: 09/01/2024
+#Assignment: Madlib.py
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input(" Give me a noun 1:")
+  noun2 = input(" Give me a noun 2:")
+  verb1 = input(" Give me a verb 1:")
+  verb2 = input(" Give me a verb 2:")
+  adjective1 = input(" Give me adjective 1:")
+  adjective2 = input(" Give me adjective 2:")
   #Print the story with the user supplied words.
-
+  print( noun1 + "met up with" + noun2 + "a" + adjective1 + "person" + "." + noun1 + verb1 + "apple pie" + "and" + noun2 + verb2 + "crawfish" + "." + noun2 + "felt" + adjective2 + ".")
 
 
 #Call the main function if this is the file being run.
